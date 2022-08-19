804262272add6a3c222579a59066631d71464b23
Pavel <vasilyev.x@mail.ru>
2022-08-19 10:57:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        var box = 3;
+        System.out.println(box);
+        box=box*14;
+        System.out.println(box);
+    }
+}
