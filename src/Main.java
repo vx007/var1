61e5b49b5b5a058d413eedf2ff8e31a3734801c3
Pavel <vasilyev.x@mail.ru>
2022-08-19 11:01:29 +0300

    Опустошить код

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        var box = 3;
-        System.out.println(box);
-        box=box*14;
-        System.out.println(box);
+
     }
 }
